52e736553688ffb8907009c950becb5f8c61fbbb
Kasturi <75318338+KasturiS899@users.noreply.github.com>
2023-07-21 23:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const heightWidth=document.getElementById("height-width")
+let w= 
+heightWidth.addEventListener("resize")
+heightWidth.innerHtml="<h1> Width: "+heightWidth.height + "and Height:"+h
+
+
